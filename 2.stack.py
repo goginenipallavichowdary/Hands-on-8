84abda884efe60a48bb49e6c7ed4779305feae4f
Gogineni Pallavi Chowdary <86236414+goginenipallavichowdary@users.noreply.github.com>
2024-10-15 23:39:50 -0500

    Update 2.stack.py

--- 2.stack.py
+++ 2.stack.py
@@ -1,31 +1,51 @@
 class Stack:
-    def __init__(self, size):
-        self.size = size
-        self.stack = [-1] * size
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.array = [0] * capacity  # Simulating C-style array
         self.top = -1
 
     def push(self, item):
-        if self.top == self.size - 1:
-            print("Stack Overflow")
-        else:
-            self.top += 1
-            self.stack[self.top] = item
+        if self.is_full():
+            raise Exception("Stack overflow")
+        self.top += 1
+        self.array[self.top] = item
 
     def pop(self):
-        if self.top == -1:
-            print("Stack Underflow")
-        else:
-            item = self.stack[self.top]
-            self.top -= 1
-            return item
+        if self.is_empty():
+            raise Exception("Stack underflow")
+        item = self.array[self.top]
+        self.top -= 1
+        return item
 
-# Example
-size = int(input("Enter the size of the stack: "))
-stack = Stack(size)
-stack.push(1)
-stack.push(2)
-print(stack.pop())
+    def peek(self):
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        return self.array[self.top]
 
-"""output
-Enter the size of the stack: 5
-2"""
+    def is_empty(self):
+        return self.top == -1
+
+    def is_full(self):
+        return self.top == self.capacity - 1
+
+    def size(self):
+        return self.top + 1
+
+# Example usage and output
+stack = Stack(5)
+stack.push(10)
+stack.push(20)
+stack.push(30)
+
+print("Stack size:", stack.size())
+print("Top element:", stack.peek())
+print("Popped:", stack.pop())
+print("New top element:", stack.peek())
+print("Stack size after pop:", stack.size())
+
+#output:
+#Stack size: 3
+#Top element: 30
+#Popped: 30
+#New top element: 20
+#Stack size after pop: 2
