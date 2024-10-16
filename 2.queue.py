06d11f840550f802881ff1503e23ff7c799391dd
Gogineni Pallavi Chowdary <86236414+goginenipallavichowdary@users.noreply.github.com>
2024-10-15 23:40:56 -0500

    Update 2.queue.py

--- 2.queue.py
+++ 2.queue.py
@@ -1,32 +1,55 @@
 class Queue:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.elements = [-1] * capacity
-        self.start = 0
-        self.end = 0
+        self.array = [0] * capacity  # Simulating C-style array
+        self.front = 0
+        self.rear = -1
+        self.size = 0
 
-    def enqueue(self, value):
-        if self.end == self.capacity:
-            print("Queue is full")
-        else:
-            self.elements[self.end] = value
-            self.end += 1
+    def enqueue(self, item):
+        if self.is_full():
+            raise Exception("Queue overflow")
+        self.rear = (self.rear + 1) % self.capacity
+        self.array[self.rear] = item
+        self.size += 1
 
     def dequeue(self):
-        if self.start == self.end:
-            print("Queue is empty")
-        else:
-            value = self.elements[self.start]
-            self.start += 1
-            return value
+        if self.is_empty():
+            raise Exception("Queue underflow")
+        item = self.array[self.front]
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return item
 
-# Example 
-capacity = int(input("Enter the size of the queue: "))
-queue = Queue(capacity)
-queue.enqueue(1)
-queue.enqueue(2)
-print(queue.dequeue())
+    def front_element(self):
+        if self.is_empty():
+            raise Exception("Queue is empty")
+        return self.array[self.front]
 
-"""output
-Enter the size of the queue: 2
-1"""
+    def is_empty(self):
+        return self.size == 0
+
+    def is_full(self):
+        return self.size == self.capacity
+
+    def get_size(self):
+        return self.size
+
+# Example usage and output
+queue = Queue(5)
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+
+print("Queue size:", queue.get_size())
+print("Front element:", queue.front_element())
+print("Dequeued:", queue.dequeue())
+print("New front element:", queue.front_element())
+print("Queue size after dequeue:", queue.get_size())
+
+#output:
+#Queue size: 3
+#Front element: 10
+#Dequeued: 10
+#New front element: 20
+#Queue size after dequeue: 2
