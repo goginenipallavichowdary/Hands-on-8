92df1e6bc4e3c1ed2745390f3681d61c2643a9e7
Gogineni Pallavi Chowdary <86236414+goginenipallavichowdary@users.noreply.github.com>
2024-10-15 21:36:22 -0500

    Update and rename queue.c to queue.py

---
+++ queue.py
@@ -0,0 +1,31 @@
+class Queue:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.elements = [-1] * capacity
+        self.start = 0
+        self.end = 0
+
+    def enqueue(self, value):
+        if self.end == self.capacity:
+            print("Queue is full")
+        else:
+            self.elements[self.end] = value
+            self.end += 1
+
+    def dequeue(self):
+        if self.start == self.end:
+            print("Queue is empty")
+        else:
+            value = self.elements[self.start]
+            self.start += 1
+            return value
+
+# Example 
+capacity = int(input("Enter the size of the queue: "))
+queue = Queue(capacity)
+queue.enqueue(1)
+queue.enqueue(2)
+print(queue.dequeue())
+
+"""Enter the size of the queue: 2
+1"""
